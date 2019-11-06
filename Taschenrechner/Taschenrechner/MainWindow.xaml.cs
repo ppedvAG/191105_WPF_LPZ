1a00315c79e7d70cb323b5118b1a996ad2a8d7cc
Michael Zöhling <michael.zoehling@outlook.com>
2019-11-06 10:01:23 +0100

    Wiederholung WPF - Taschenrechner

---
+++ Taschenrechner/Taschenrechner/MainWindow.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Taschenrechner
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void labelClose_Click(object sender, MouseButtonEventArgs e)
+        {
+            // Schließen einer WPF-Anwendung
+            Close();
+        }
+
+        private void MoveWindow(object sender, MouseButtonEventArgs e)
+        {
+            if(e.ButtonState == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+
+        private void buttonAddieren_Click(object sender, RoutedEventArgs e)
+        {
+            // float und double: Zahlen auf Basis 2
+            // decimal: Zahlen auf Basis 10  ---> Faktor 50 langsamer
+
+            // Genaue Angabe des Styles :
+            //decimal demo = decimal.Parse("123.123,132321322", NumberStyles.AllowThousands | NumberStyles.AllowDecimalPoint);
+
+            decimal zahl1 = Convert.ToDecimal(textBoxZahl1.Text,Thread.CurrentThread.CurrentCulture); // de-AT
+            decimal zahl2 = Convert.ToDecimal(textBoxZahl2.Text,Thread.CurrentThread.CurrentCulture); // de-AT
+
+            labelErgebnis.Content = zahl1 + zahl2;
+        }
+
+        private void buttonSubtrahieren_Click(object sender, RoutedEventArgs e)
+        {
+            decimal zahl1 = Convert.ToDecimal(textBoxZahl1.Text, Thread.CurrentThread.CurrentCulture); // de-AT
+            decimal zahl2 = Convert.ToDecimal(textBoxZahl2.Text, Thread.CurrentThread.CurrentCulture); // de-AT
+
+            labelErgebnis.Content = zahl1 - zahl2;
+        }
+
+        private void buttonMultiplizieren_Click(object sender, RoutedEventArgs e)
+        {
+            decimal zahl1 = Convert.ToDecimal(textBoxZahl1.Text, Thread.CurrentThread.CurrentCulture); // de-AT
+            decimal zahl2 = Convert.ToDecimal(textBoxZahl2.Text, Thread.CurrentThread.CurrentCulture); // de-AT
+
+            labelErgebnis.Content = zahl1 * zahl2;
+        }
+
+        private void buttonDividieren_Click(object sender, RoutedEventArgs e)
+        {
+            if(textBoxZahl2.Text == "0")
+            {
+                labelErgebnis.Content = "Division durch Null !!!!";
+                return;
+            }
+
+            decimal zahl1 = Convert.ToDecimal(textBoxZahl1.Text, Thread.CurrentThread.CurrentCulture); // de-AT
+            decimal zahl2 = Convert.ToDecimal(textBoxZahl2.Text, Thread.CurrentThread.CurrentCulture); // de-AT
+
+            labelErgebnis.Content = zahl1 / zahl2;
+        }
+    }
+}
