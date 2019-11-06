d2c14e54f685ae7f50f8e847712befb6961abde9
Michael Zöhling <michael.zoehling@outlook.com>
2019-11-06 14:38:39 +0100

    Eigener Konverter

---
+++ Binding_Demo/Binding_Demo/Farbkonverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace Binding_Demo
+{
+    public class Farbkonverter : IValueConverter // Schnittstelle für Konverter
+    {
+        // OneWay-Binding
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (string.IsNullOrWhiteSpace(value.ToString()))
+                return Brushes.Black;
+
+            string wert = (string)value;
+            if (wert == "Rot")
+                return Brushes.Red;
+            else if (wert == "Gelb")
+                return Brushes.Yellow;
+            else
+                return Brushes.Blue;
+        }
+
+        // TwoWay-Binding
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
