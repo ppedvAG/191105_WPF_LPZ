344994b1632bdd8df3b268e0adc7776b57ed9dda
Michael Zöhling <michael.zoehling@outlook.com>
2019-11-06 12:18:03 +0100

    Listbox mit eigenem Template

--- ListenTemplates/ListenTemplates/MainWindow.xaml.cs
+++ ListenTemplates/ListenTemplates/MainWindow.xaml.cs
@@ -24,5 +24,50 @@ namespace ListenTemplates
         {
             InitializeComponent();
         }
+
+        private void buttonEinfügen_Click(object sender, RoutedEventArgs e)
+        {
+            Random r = new Random();
+            var wert = r.Next(0, 3);
+
+            Person p1 = null;
+            if (wert == 1)
+            {
+                p1 = new Person
+                {
+                    Vorname = "Tom",
+                    Nachname = "Ate",
+                    Alter = 10,
+                    Kontostand = 100,
+                    ImageURL = "http://ais.bz-ticket.de/piece/09/4a/b8/7a/155891834-f-3_2-w-980.jpg"
+                };
+            }
+            else if (wert == 2)
+            {
+                p1 = new Mitarbeiter
+                {
+                    MitarbeiterID = 12,
+                    Vorname = "Anna",
+                    Nachname = "Nass",
+                    Alter = 20,
+                    Kontostand = 200,
+                    ImageURL = "https://images-na.ssl-images-amazon.com/images/I/81mzRlPT4gL._SY445_.jpg"
+                };
+            }
+            else
+            {
+                p1 = new Kunde
+                {
+                    Stammkunde = true,
+                    Vorname = "Peter",
+                    Nachname = "Silie",
+                    Alter = 30,
+                    Kontostand = 3000,
+                    ImageURL = "https://www.ndr.de/ratgeber/kochen/warenkunde/petersilie109_v-contentxl.jpg"
+                };
+            }
+
+            listBoxPersonen.Items.Add(p1);
+        }
     }
 }
---
+++ ListenTemplates/ListenTemplates/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListenTemplates
+{
+    class Person
+    {
+        public string Vorname { get; set; }
+        public string Nachname { get; set; }
+        public byte Alter { get; set; }
+        public decimal Kontostand { get; set; }
+
+        public string ImageURL { get; set; }
+    }
+
+    class Mitarbeiter : Person
+    {
+        public int MitarbeiterID { get; set; }
+    }
+
+    class Kunde : Person
+    {
+        public bool Stammkunde { get; set; }
+    }
+}
