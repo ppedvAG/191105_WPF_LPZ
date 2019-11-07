e48a14884bb8587eb6dab0a01bee5fbe8c2dc8fc
Michael Zöhling <michael.zoehling@outlook.com>
2019-11-07 14:25:58 +0100

    MVVM mit 2 Oberflächen (Grid und Dockpanel)

---
+++ MVVM_Simpel/MVVM_Simpel/ViewModel/ButtonCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace MVVM_Simpel.ViewModel
+{
+    public class ButtonCommand : ICommand
+    {
+        public ButtonCommand(Action execute, Func<bool> canExecute = null)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute ?? new Func<bool>(() => true); // Keine Validierung -> Alles ist erlaubt :)
+        }
+
+        private Action execute;
+        private Func<bool> canExecute;
+        
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            // Validierung
+            return canExecute();
+        }
+
+        public void Execute(object parameter)
+        {
+            // Logik ausführen
+            execute();
+        }
+    }
+}
---
+++ MVVM_Simpel/MVVM_Simpel/ViewModel/MainViewModel.cs
@@ -0,0 +1,32 @@
+﻿using MVVM_Simpel.Model;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MVVM_Simpel.ViewModel
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public MainViewModel()
+        {
+            GetPersonenCommand = new ButtonCommand(GetPersonen);
+        }
+
+        private void GetPersonen()
+        {
+            Personenliste = service.GetPersonen();
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Personenliste)));
+        }
+
+        // Alle Services:
+        private PersonenService service = new PersonenService();
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public List<Person> Personenliste { get; set; }
+        public ICommand GetPersonenCommand { get; set; }
+    }
+}
