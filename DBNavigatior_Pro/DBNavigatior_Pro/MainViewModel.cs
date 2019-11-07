8da2d7653074f2e4cc53acb019c9b6eb3d1ae368
Michael Zöhling <michael.zoehling@outlook.com>
2019-11-07 15:27:09 +0100

    Rohbau ViewModel

---
+++ DBNavigatior_Pro/DBNavigatior_Pro/MainViewModel.cs
@@ -0,0 +1,57 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace DBNavigatior_Pro
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public MainViewModel()
+        {
+            GetLocationCommand = new RelayCommand(GetLocation);
+            GetJourneyDetailsCommand = new RelayCommand(GetJourneyDetails);
+            GetDepartureBoardsForLocationCommand = new RelayCommand(GetDepartureBoardsFromLocation);
+
+            service = new DBService();
+        }
+
+        private DBService service;
+
+        private void GetDepartureBoardsFromLocation()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void GetJourneyDetails()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void GetLocation()
+        {
+            throw new NotImplementedException();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        // Elemente, die in der Oberfläche ausgewählt sind
+        // -> TwoWayBinding !!! (mindestens: OneWayToSource)
+        public string SearchString { get; set; }
+        public Location CurrentLocation { get; set; }
+        public DepartureBoard CurrentDepartureBoard { get; set; }
+
+        public List<Location> Locations { get; set; }
+        public List<DepartureBoard> DepartureBoards { get; set; }
+        public List<JourneyDetails> JourneyDetails { get; set; }
+
+
+        public ICommand GetLocationCommand { get; set; }
+        public ICommand GetDepartureBoardsForLocationCommand { get; set; }
+        public ICommand GetJourneyDetailsCommand { get; set; }
+    }
+}
---
+++ DBNavigatior_Pro/DBNavigatior_Pro/RelayCommand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace DBNavigatior_Pro
+{
+    public class RelayCommand : ICommand
+    {
+        // Quickactions+Refactoring/GenerateConstructor
+        public RelayCommand(Action execute, Func<bool> canExecute = null)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute ?? new Func<bool>(() => true);
+        }
+
+        private Action execute;
+        private Func<bool> canExecute;
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return canExecute();
+        }
+
+        public void Execute(object parameter)
+        {
+            return execute();
+        }
+    }
+}
