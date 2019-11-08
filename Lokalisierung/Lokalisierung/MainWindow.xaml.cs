b2bcb69f16ec5f78214a4ba518c9f8c40b17c6aa
Michael Zöhling <michael.zoehling@outlook.com>
2019-11-08 14:23:17 +0100

    Lokalisierung mit .resx - Datein

--- Lokalisierung/Lokalisierung/MainWindow.xaml.cs
+++ Lokalisierung/Lokalisierung/MainWindow.xaml.cs
@@ -24,5 +24,11 @@ namespace Lokalisierung
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // Lokalisierung im Code
+            MessageBox.Show(Properties.Resources.windowTitle);
+        }
     }
 }
